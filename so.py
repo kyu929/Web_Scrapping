ce4ff2285d0ea3c25019079187ccf26bd89cb999
kyu929 <j2mity@gmail.com>
2020-08-18 23:41:57 +0900

    Modified extract_job(AttributeError)

--- so.py
+++ so.py
@@ -13,21 +13,27 @@ def get_last_page():
 
 
 def extract_job(html):
-    title = html.find("div", {"class" : "-title"}).find("h2").find("a")["title"]
-    company, location = html.find("div", {"class":"-company"}).find_all("span", recursive=False)
-
-    return {'title':title}
+    title = html.find("h2", {"class" : "fs-body3"}).find("a")["title"]
+    company, location = html.find("div", {
+        "class":"-company"
+        }).find_all(
+            "span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True).strip("-").strip(" \r")
+    job_id = html['data-jobid' ]
+    return {'title':title, 'company':company, 'location':location, 'apply_link': f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO page {page}")
         result = requests.get(f"{URL}&pg={page+1}")
-        soup = BeautifulSoup(result.text, "html.parse")
+        soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class":"-job"})
         for result in results:
             job = extract_job(result)
-            jobs.append(jobs)
+            jobs.append(job)
     return jobs
 
 
